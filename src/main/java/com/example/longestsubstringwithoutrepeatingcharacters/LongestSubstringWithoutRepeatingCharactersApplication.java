22e2dcb23227b6c1a042cf7fac0ec13cf75f900b
Jérôme Guèrin <guerin_jerome@icloud.com>
2022-07-08 21:59:48 +0200

    Initial commit

---
+++ src/main/java/com/example/longestsubstringwithoutrepeatingcharacters/LongestSubstringWithoutRepeatingCharactersApplication.java
@@ -0,0 +1,13 @@
+package com.example.longestsubstringwithoutrepeatingcharacters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongestSubstringWithoutRepeatingCharactersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LongestSubstringWithoutRepeatingCharactersApplication.class, args);
+	}
+
+}

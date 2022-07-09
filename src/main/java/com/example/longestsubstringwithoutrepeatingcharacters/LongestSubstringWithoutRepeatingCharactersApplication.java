e502dc7ef7e29a374e0582c48f4be081e928559f
Jérôme Guèrin <guerin_jerome@icloud.com>
2022-07-09 14:38:49 +0200

    feat/implement algorithm and tests

--- src/main/java/com/example/longestsubstringwithoutrepeatingcharacters/LongestSubstringWithoutRepeatingCharactersApplication.java
+++ src/main/java/com/example/longestsubstringwithoutrepeatingcharacters/LongestSubstringWithoutRepeatingCharactersApplication.java
@@ -3,6 +3,8 @@ package com.example.longestsubstringwithoutrepeatingcharacters;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class LongestSubstringWithoutRepeatingCharactersApplication {
 
@@ -10,4 +12,29 @@ public class LongestSubstringWithoutRepeatingCharactersApplication {
 		SpringApplication.run(LongestSubstringWithoutRepeatingCharactersApplication.class, args);
 	}
 
+	public static int findTheLengthOfTheLongestSubstring(String string) {
+		List<String> characters = string.chars()
+				.mapToObj(c -> String.valueOf((char) c))
+				.toList();
+
+		String longestSubtring = "";
+		String currentSubstring = "";
+		int baseIndex = 0;
+
+		for (int i = 0; i < characters.size(); i++) {
+			String letter = characters.get(i);
+			if (!currentSubstring.contains(letter)) {
+				currentSubstring = currentSubstring.concat(letter);
+				if (currentSubstring.length() > longestSubtring.length()) {
+					longestSubtring = currentSubstring;
+				}
+			} else {
+				i = baseIndex;
+				baseIndex++;
+				currentSubstring = "";
+			}
+		}
+
+		return longestSubtring.length();
+	}
 }
--- src/test/java/com/example/longestsubstringwithoutrepeatingcharacters/LongestSubstringWithoutRepeatingCharactersApplicationTests.java
+++ src/test/java/com/example/longestsubstringwithoutrepeatingcharacters/LongestSubstringWithoutRepeatingCharactersApplicationTests.java
@@ -1,13 +1,59 @@
 package com.example.longestsubstringwithoutrepeatingcharacters;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class LongestSubstringWithoutRepeatingCharactersApplicationTests {
 
 	@Test
-	void contextLoads() {
+	@DisplayName("Valeur abcabcbb égale 3")
+	public void algorithmLSWRC_abcabcbb_3() {
+		String value = "abcabcbb";
+		int result = LongestSubstringWithoutRepeatingCharactersApplication.findTheLengthOfTheLongestSubstring(value);
+		assertEquals(3, result);
 	}
 
+	@Test
+	@DisplayName("Valeur bbbbb égale 1")
+	public void algorithmLSWRC_bbbbb_1() {
+		String value = "bbbbb";
+		int result = LongestSubstringWithoutRepeatingCharactersApplication.findTheLengthOfTheLongestSubstring(value);
+		assertEquals(1, result);
+	}
+
+	@Test
+	@DisplayName("Valeur pwwkew égale 3")
+	public void algorithmLSWRC_pwwkew_3() {
+		String value = "pwwkew";
+		int result = LongestSubstringWithoutRepeatingCharactersApplication.findTheLengthOfTheLongestSubstring(value);
+		assertEquals(3, result);
+	}
+
+	@Test
+	@DisplayName("Valeur cdd égale 2")
+	public void algorithmLSWRC_cdd_2() {
+		String value = "cdd";
+		int result = LongestSubstringWithoutRepeatingCharactersApplication.findTheLengthOfTheLongestSubstring(value);
+		assertEquals(2, result);
+	}
+
+	@Test
+	@DisplayName("Valeur bdb égale 2")
+	public void algorithmLSWRC_bdb_2() {
+		String value = "bdb";
+		int result = LongestSubstringWithoutRepeatingCharactersApplication.findTheLengthOfTheLongestSubstring(value);
+		assertEquals(2, result);
+	}
+
+	@Test
+	@DisplayName("Valeur dvdf égale 3")
+	public void algorithmLSWRC_dvdf_3() {
+		String value = "dvdf";
+		int result = LongestSubstringWithoutRepeatingCharactersApplication.findTheLengthOfTheLongestSubstring(value);
+		assertEquals(3, result);
+	}
 }
